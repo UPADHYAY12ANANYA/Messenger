a366e440024027294149133137bfdd34ea33e718
UPADHYAY12ANANYA <ananya9662upadhyay@gmail.com>
2024-10-24 09:20:46 +0530

    Updates

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Chat.css'
+import Cam from "../../assets/cam.png"
+import Add from "../../assets/add.png"
+import More from "../../assets/more.png"
+import Messages from '../Messages/Messages'
+import Input from '../Input/Input'
 
 function Chat(){
     return(
-        <div className='chat'>Chat</div>
+        <div className='chat'>
+            <div className="chatInfo">
+                <span>Jane</span>
+                <div className="chatIcons">
+                    <img src={Cam} alt="Cam" />
+                    <img src={Add} alt="Add" />
+                    <img src={More} alt="More" />
+                </div>
+            </div>
+            <Messages />
+            <Input />
+        </div>
     )
 }
 
